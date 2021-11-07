886701d1df8f7ca710a1dcf60c3d480dc4149a90
wenqing <bbb>
2021-11-07 19:13:32 +0800

    位运算 电路

---
+++ leetcode/editor/cn/[137]只出现一次的数字 II.java
@@ -0,0 +1,59 @@
+/**
+<p>给你一个整数数组 <code>nums</code> ，除某个元素仅出现 <strong>一次</strong> 外，其余每个元素都恰出现 <strong>三次 。</strong>请你找出并返回那个只出现了一次的元素。</p>
+
+<p> </p>
+
+<p><strong>示例 1：</strong></p>
+
+<pre>
+<strong>输入：</strong>nums = [2,2,3,2]
+<strong>输出：</strong>3
+</pre>
+
+<p><strong>示例 2：</strong></p>
+
+<pre>
+<strong>输入：</strong>nums = [0,1,0,1,0,1,99]
+<strong>输出：</strong>99
+</pre>
+
+<p> </p>
+
+<p><strong>提示：</strong></p>
+
+<ul>
+	<li><code>1 <= nums.length <= 3 * 10<sup>4</sup></code></li>
+	<li><code>-2<sup>31</sup> <= nums[i] <= 2<sup>31</sup> - 1</code></li>
+	<li><code>nums</code> 中，除某个元素仅出现 <strong>一次</strong> 外，其余每个元素都恰出现 <strong>三次</strong></li>
+</ul>
+
+<p> </p>
+
+<p><strong>进阶：</strong>你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？</p>
+<div><div>Related Topics</div><div><li>位运算</li><li>数组</li></div></div><br><div><li>👍 757</li><li>👎 0</li></div>
+*/
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int singleNumber(int[] nums) {
+//		int ans = 0;
+//		for(int i = 0; i < 32; i++)
+//		{
+//			int total = 0;
+//			for(int x : nums)
+//			{
+//				total += (x >> i) & 1;
+//			}
+//			if(total % 3 != 0) ans+= (1 << i);
+//		}
+//		return ans;
+		int a=0, b = 0;
+		for(int x : nums)
+		{
+			b = ~a & (b ^ x);
+			a = ~b & (a ^ x);
+		}
+		return b;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
