378b3ccdb3fd1bc1d9f6bcb478f78cc1db501024
wenqing <bbb>
2021-11-28 10:24:29 +0800

    字符串 哈希

---
+++ leetcode/editor/cn/[438]找到字符串中所有字母异位词.java
@@ -0,0 +1,80 @@
+/**
+<p>给定两个字符串&nbsp;<code>s</code>&nbsp;和 <code>p</code>，找到&nbsp;<code>s</code><strong>&nbsp;</strong>中所有&nbsp;<code>p</code><strong>&nbsp;</strong>的&nbsp;<strong>异位词&nbsp;</strong>的子串，返回这些子串的起始索引。不考虑答案输出的顺序。</p>
+
+<p><strong>异位词 </strong>指由相同字母重排列形成的字符串（包括相同的字符串）。</p>
+
+<p>&nbsp;</p>
+
+<p><strong>示例&nbsp;1:</strong></p>
+
+<pre>
+<strong>输入: </strong>s = "cbaebabacd", p = "abc"
+<strong>输出: </strong>[0,6]
+<strong>解释:</strong>
+起始索引等于 0 的子串是 "cba", 它是 "abc" 的异位词。
+起始索引等于 6 的子串是 "bac", 它是 "abc" 的异位词。
+</pre>
+
+<p><strong>&nbsp;示例 2:</strong></p>
+
+<pre>
+<strong>输入: </strong>s = "abab", p = "ab"
+<strong>输出: </strong>[0,1,2]
+<strong>解释:</strong>
+起始索引等于 0 的子串是 "ab", 它是 "ab" 的异位词。
+起始索引等于 1 的子串是 "ba", 它是 "ab" 的异位词。
+起始索引等于 2 的子串是 "ab", 它是 "ab" 的异位词。
+</pre>
+
+<p>&nbsp;</p>
+
+<p><strong>提示:</strong></p>
+
+<ul>
+	<li><code>1 &lt;= s.length, p.length &lt;= 3 * 10<sup>4</sup></code></li>
+	<li><code>s</code>&nbsp;和&nbsp;<code>p</code>&nbsp;仅包含小写字母</li>
+</ul>
+<div><div>Related Topics</div><div><li>哈希表</li><li>字符串</li><li>滑动窗口</li></div></div><br><div><li>👍 666</li><li>👎 0</li></div>
+*/
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> list = new ArrayList<>();
+        int sLen = s.length(), pLen = p.length();
+        if(sLen < pLen) return list;
+        int[] res = new int[26];
+        for(int i = 0; i < pLen; i++)
+        {
+            --res[p.charAt(i) - 'a'];
+            ++res[s.charAt(i) - 'a'];
+        }
+        int diff = 0;
+        for(int i = 0; i < 26; i++)
+            if(res[i] != 0) diff++;
+        if(diff == 0) list.add(0);
+        for (int i = 0; i < sLen - pLen;i++)
+        {
+            if(res[s.charAt(i) - 'a'] == 0)
+            {
+                diff++;
+            }else if(res[s.charAt(i) - 'a'] == 1)
+            {
+                diff--;
+            }
+            --res[s.charAt(i) - 'a'];
+            if(res[s.charAt(i + pLen) - 'a'] == 0)
+            {
+                diff++;
+            }
+            else if(res[s.charAt(i + pLen) - 'a'] == -1)
+            {
+                diff--;
+            }
+            ++res[s.charAt(i + pLen) - 'a'];
+            if(diff == 0) list.add(i + 1);
+        }
+        return list;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
